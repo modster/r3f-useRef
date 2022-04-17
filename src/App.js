43d17bbf937a93110f141bceb964cf0eef18b574
E. M. Greeff <unmodster@gmail.com>
2022-04-17 09:01:30 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+import "./styles.css";
+
+function MyRotatingBox() {
+  const myMesh = React.useRef();
+
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime();
+    myMesh.current.rotation.x = a;
+  });
+
+  return (
+    <mesh ref={myMesh}>
+      <boxBufferGeometry />
+      <meshPhongMaterial color="royalblue" />
+    </mesh>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <Canvas>
+        <MyRotatingBox />
+        <ambientLight intensity={0.1} />
+        <directionalLight />
+      </Canvas>
+    </div>
+  );
+}
